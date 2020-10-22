22325711f32f87372731f26501ca0aac4d8840bd
AdrianIgl <71347903+AdrianIgl@users.noreply.github.com>
2020-10-22 16:37:03 +0200

    Add files via upload

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@ echo '<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <title>MENU</title>
-   
+    <link href = "css/main.css" rel = "stylesheet">
 </head>
 <body>
     <div id="menu">
